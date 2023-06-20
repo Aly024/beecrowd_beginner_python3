ca30b3d56ab51e58ad8eed85eb2c5d4fc3001edd
Aly Azfar <46001662+Aly024@users.noreply.github.com>
2023-06-20 21:28:03 +0600

    Update 1037.py

--- 1037.py
+++ 1037.py
@@ -1,4 +1,4 @@
-'''
+
 val = float(input())
 
 lb = [0, 25, 50, 75]
@@ -25,19 +25,4 @@ while i in range(4):
 
     i += 1
 
-'''
 
-# Interval
-
-a = float(input())
-
-if a<0 or a>100 :
-    print('Fora de intervalo')
-elif a<=25:
-    print('Intervalo [0,25]')
-elif a<=50 :
-    print('Intervalo (25,50]')
-elif a<=75:
-    print('Intervalo (50,75]')
-else :
-    print('Intervalo (75,100]')
